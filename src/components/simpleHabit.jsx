bc24511396bfcaae599a23fa039100cfdc9c8602
dream-ellie <dream.coder.ellie@gmail.com>
2020-09-09 10:34:17 +1000

    Add SimpleHabit

---
+++ src/components/simpleHabit.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useRef, useCallback, useEffect } from 'react';
+
+const SimpleHabit = () => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+
+  useEffect(() => {
+    console.log(`mounted & updated!: ${count}`);
+  }, []);
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">
+        Reading
+      </span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
+
+export default SimpleHabit;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root')
 );
