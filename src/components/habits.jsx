6b622f8cf31b6463956f9a2e87fd2fe2e8365084
Ellie <dream.coder.ellie@gmail.com>
2021-02-05 21:48:36 +0900

    Clean up Habits Component

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,35 +3,19 @@ import Habit from './habit';
 import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-  handleIncrement = habit => {
-    this.props.onIncrement(habit);
-  };
-
-  handleDecrement = habit => {
-    this.props.onDecrement(habit);
-  };
-
-  handleDelete = habit => {
-    this.props.onDelete(habit);
-  };
-
-  handleAdd = name => {
-    this.props.onAdd(name);
-  };
-
   render() {
     console.log('habits');
     return (
       <div className="habits">
-        <HabitAddForm onAdd={this.handleAdd} />
+        <HabitAddForm onAdd={this.props.onAdd} />
         <ul>
           {this.props.habits.map(habit => (
             <Habit
               key={habit.id}
               habit={habit}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
             />
           ))}
         </ul>
